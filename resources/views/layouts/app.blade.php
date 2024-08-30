f404912e6efcc0e530c9be40ee39f11c24b9cae7
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-08-30 15:12:25 +0700

    instalasi laravel, asset skillgrow, layouting

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+
+<!-- Mirrored from html.themegenix.com/skillgro/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 30 Aug 2024 06:41:22 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>SkillGro - Online Courses & Education Template</title>
+    <meta name="description" content="SkillGro - Online Courses & Education Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/animate.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/fontawesome-all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/flaticon-skillgro.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/flaticon-skillgro-new.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/swiper-bundle.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/default-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/odometer.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/aos.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/plyr.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/spacing.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/tg-cursor.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
+
+    @yield('style')
+</head>
+
+<body>
+
+    <!--Preloader-->
+    <div id="preloader">
+        <div id="loader" class="loader">
+            <div class="loader-container">
+                <div class="loader-icon"><img src="{{ asset('assets/img/logo/preloader.svg') }}" alt="Preloader"></div>
+            </div>
+        </div>
+    </div>
+    <!--Preloader-end -->
+
+    <!-- Scroll-top -->
+    <button class="scroll__top scroll-to-target" data-target="html">
+        <i class="tg-flaticon-arrowhead-up"></i>
+    </button>
+    <!-- Scroll-top-end-->
+
+    <!-- header-area -->
+        @include('layouts.header')
+    <!-- header-area-end -->
+
+
+
+    <!-- main-area -->
+    <main class="main-area fix">
+        @yield('content')
+    </main>
+    <!-- main-area-end -->
+
+
+
+    <!-- footer-area -->
+        @include('layouts.footer')
+    <!-- footer-area-end -->
+
+
+
+
+    <!-- JS here -->
+    <script src="{{ asset('assets/js/vendor/jquery-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/imagesloaded.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.odometer.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.appear.js') }}"></script>
+    <script src="{{ asset('assets/js/tween-max.min.js') }}"></script>
+    <script src="{{ asset('assets/js/select2.min.js') }}"></script>
+    <script src="{{ asset('assets/js/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.marquee.min.js') }}"></script>
+    <script src="{{ asset('assets/js/tg-cursor.min.js') }}"></script>
+    <script src="{{ asset('assets/js/vivus.min.js') }}"></script>
+    <script src="{{ asset('assets/js/ajax-form.js') }}"></script>
+    <script src="{{ asset('assets/js/svg-inject.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.circleType.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.lettering.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plyr.min.js') }}"></script>
+    <script src="{{ asset('assets/js/wow.min.js') }}"></script>
+    <script src="{{ asset('assets/js/aos.js') }}"></script>
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+    <script>
+        SVGInject(document.querySelectorAll("img.injectable"));
+    </script>
+
+    @yield('script')
+</body>
+
+
+<!-- Mirrored from html.themegenix.com/skillgro/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 30 Aug 2024 06:42:43 GMT -->
+</html>
